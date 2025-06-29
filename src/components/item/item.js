6e6ba3b446629cb623edae403f6c359d18623169
Adanta Griffin <griffinadanta90@gmail.com>
2025-06-29 10:09:36 -0400

    deploy bug

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -5,7 +5,7 @@ function Item(props){
     return (
         <div className={styles.container}>
             <div className={styles['item-information']}>
-                <img className={styles.img}  src={props.item.image} alt="image"/>
+                <img className={styles.img}  src={props.item.image}/>
                 <h3 className={styles.h3}> {props.item.name}</h3>
                 <h4 className={styles.h4}> {props.item.album}</h4>
                 <p className={styles.p}> {props.item.song}</p>
